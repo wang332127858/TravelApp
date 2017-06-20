ab39c0f1786cff8adf83a48ef52d85ac7b7829f0
wang332127858 <332127858@qq.com>
2017-06-20 09:37:24 +0800

    zxh 提交ILevelRuleService

---
+++ BaoZhongTravel/BaoZhong.IServices/BaoZhong.IServices/ILevelRuleService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using BaoZhong.Model;
+using BaoZhong.IServices.QueryModel;
+
+namespace BaoZhong.IServices
+{
+    public interface ILevelRuleService : IService, IDisposable
+    {
+        void AddLevelRule(LevelRuleInfo model);
+
+        void UpdateLevelRule(LevelRuleInfo model);
+
+        void DeleteLevelRule(long id);
+
+        IQueryable<LevelRuleInfo> GetLevelRules();
+    }
+}
