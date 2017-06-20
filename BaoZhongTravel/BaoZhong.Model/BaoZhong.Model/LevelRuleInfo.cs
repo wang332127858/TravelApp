d3473137baf093a1b0f0e1ba1ca88b2c14934ba5
wang332127858 <332127858@qq.com>
2017-06-20 09:20:37 +0800

    zxh 提交LevelRuleInfo，SupplierInfo

---
+++ BaoZhongTravel/BaoZhong.Model/BaoZhong.Model/LevelRuleInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BaoZhong.Model
+{
+    public class LevelRuleInfo : BaseModel
+    {
+        private long _id;
+
+        public new long Id
+        {
+            get
+            {
+                return this._id;
+            }
+            set
+            {
+                this._id = value;
+                base.Id = value;
+            }
+        }
+
+        public long UserLevel
+        {
+            get;
+            set;
+        }
+
+        public long BeginPoint
+        {
+            get;
+            set;
+        }
+
+        public long EndPoint
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ BaoZhongTravel/BaoZhong.Model/BaoZhong.Model/SupplierInfo.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BaoZhong.Model
+{
+    public class SupplierInfo :BaseModel
+    {
+        private long _id;
+
+        public new long Id
+        {
+            get
+            {
+                return this._id;
+            }
+            set
+            {
+                this._id = value;
+                base.Id = value;
+            }
+        }
+
+        public string SupplierId
+        {
+            get;
+            set;
+        }
+
+        public string SupplierName
+        {
+            get;
+            set;
+        }
+
+        public string TravelName
+        {
+            get;
+            set;
+        }
+
+        public string LeaderName
+        {
+            get;
+            set;
+        }
+
+        public string LeaderPhone
+        {
+            get;
+            set;
+        }
+
+        public string SellerName
+        {
+            get;
+            set;
+        }
+
+        public long ContactWay
+        {
+            get;
+            set;
+        }
+
+
+        public string SellerContact
+        {
+            get;
+            set;
+        }
+
+        public string Remark
+        {
+            get;
+            set;
+        }
+
+
+
+        public string TravelAddress
+        {
+            get;
+            set;
+        }
+
+        public string LineId
+        {
+            get;
+            set;
+        }
+
+        public string LineName
+        {
+            get;
+            set;
+        }
+
+        public string RegionId
+        {
+            get;
+            set;
+        }
+
+        public string RegionName
+        {
+            get;
+            set;
+        }
+    }
+}
