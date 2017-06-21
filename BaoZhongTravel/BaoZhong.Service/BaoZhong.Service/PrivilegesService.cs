625f15a46a29c69d0f843c7562642825983330f1
wang332127858 <332127858@qq.com>
2017-06-21 09:37:39 +0800

    zxh 修改权限服务接口

--- BaoZhongTravel/BaoZhong.Service/BaoZhong.Service/PrivilegesService.cs
+++ BaoZhongTravel/BaoZhong.Service/BaoZhong.Service/PrivilegesService.cs
@@ -16,7 +16,7 @@ namespace BaoZhong.Service
 			{
 				model.Description = model.RoleName;
 			}
-			bool flag = this.context.RoleInfo.Any((RoleInfo a) => a.RoleName == model.RoleName && a.ShopId == model.ShopId);
+			bool flag = this.context.RoleInfo.Any((RoleInfo a) => a.RoleName == model.RoleName);
 			if (flag)
 			{
 				throw new BaoZhongException("已存在相同名称的权限组");
@@ -27,12 +27,12 @@ namespace BaoZhong.Service
 
 		public void UpdatePlatformRole(RoleInfo model)
 		{
-			RoleInfo updatemodel = this.context.RoleInfo.FindBy((RoleInfo a) => a.ShopId == 0L && a.Id == model.Id).FirstOrDefault<RoleInfo>();
+			RoleInfo updatemodel = this.context.RoleInfo.FindBy((RoleInfo a) => a.Id == model.Id).FirstOrDefault<RoleInfo>();
 			if (updatemodel == null)
 			{
 				throw new BaoZhongException("找不到该权限组");
 			}
-			bool flag = this.context.RoleInfo.Any((RoleInfo a) => a.RoleName == model.RoleName && a.ShopId == 0L && a.RoleName != updatemodel.RoleName);
+			bool flag = this.context.RoleInfo.Any((RoleInfo a) => a.RoleName == model.RoleName && a.RoleName != updatemodel.RoleName);
 			if (flag)
 			{
 				throw new BaoZhongException("已存在相同名称的权限组");
@@ -43,38 +43,14 @@ namespace BaoZhong.Service
 			{
 				updatemodel.Description = model.RoleName;
 			}
-			this.context.RolePrivilegeInfo.RemoveRange(updatemodel.RolePrivilegeInfo);
-			updatemodel.RolePrivilegeInfo = model.RolePrivilegeInfo;
 			this.context.SaveChanges();
 		}
 
-		public void UpdateSellerRole(RoleInfo model)
-		{
-			RoleInfo updatemodel = this.context.RoleInfo.FindBy((RoleInfo a) => a.ShopId == model.ShopId && a.Id == model.Id).FirstOrDefault<RoleInfo>();
-			if (updatemodel == null)
-			{
-				throw new BaoZhongException("找不到该权限组");
-			}
-			bool flag = this.context.RoleInfo.Any((RoleInfo a) => a.RoleName == model.RoleName && a.ShopId == model.ShopId && a.RoleName != updatemodel.RoleName);
-			if (flag)
-			{
-				throw new BaoZhongException("已存在相同名称的权限组");
-			}
-			updatemodel.RoleName = model.RoleName;
-			updatemodel.Description = model.Description;
-			if (string.IsNullOrEmpty(model.Description))
-			{
-				updatemodel.Description = model.RoleName;
-			}
-			this.context.RolePrivilegeInfo.RemoveRange(updatemodel.RolePrivilegeInfo);
-			updatemodel.RolePrivilegeInfo = model.RolePrivilegeInfo;
-			this.context.SaveChanges();
-		}
 
 		public void DeletePlatformRole(long id)
 		{
 			RoleInfo entity = (from a in this.context.RoleInfo
-			where a.Id == id && a.ShopId == 0L
+			where a.Id == id
 			select a).FirstOrDefault<RoleInfo>();
 			this.context.RoleInfo.Remove(entity);
 			this.context.SaveChanges();
@@ -83,51 +59,15 @@ namespace BaoZhong.Service
 		public RoleInfo GetPlatformRole(long id)
 		{
 			return (from a in this.context.RoleInfo
-			where a.Id == id && a.ShopId == 0L
+			where a.Id == id
 			select a).FirstOrDefault<RoleInfo>();
 		}
 
-		public RoleInfo GetSellerRole(long id, long shopid)
-		{
-			return (from a in this.context.RoleInfo
-			where a.Id == id && a.ShopId == shopid
-			select a).FirstOrDefault<RoleInfo>();
-		}
 
 		public IQueryable<RoleInfo> GetPlatformRoles()
 		{
-			return this.context.RoleInfo.FindBy((RoleInfo item) => item.ShopId == 0L);
+			return this.context.RoleInfo.FindAll();
 		}
 
-		public IQueryable<RoleInfo> GetSellerRoles(long shopId)
-		{
-			return from item in this.context.RoleInfo
-			where item.ShopId == shopId && item.ShopId != 0L
-			select item;
-		}
-
-		public void AddSellerRole(RoleInfo model)
-		{
-			if (string.IsNullOrEmpty(model.Description))
-			{
-				model.Description = model.RoleName;
-			}
-			bool flag = this.context.RoleInfo.Any((RoleInfo a) => a.RoleName == model.RoleName && a.ShopId == model.ShopId);
-			if (flag)
-			{
-				throw new BaoZhongException("已存在相同名称的权限组");
-			}
-			this.context.RoleInfo.Add(model);
-			this.context.SaveChanges();
-		}
-
-		public void DeleteSellerRole(long id, long shopId)
-		{
-			RoleInfo entity = (from a in this.context.RoleInfo
-			where a.Id == id && a.ShopId == shopId
-			select a).FirstOrDefault<RoleInfo>();
-			this.context.RoleInfo.Remove(entity);
-			this.context.SaveChanges();
-		}
 	}
 }
